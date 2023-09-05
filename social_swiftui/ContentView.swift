bf310637076f44e0fa597ac12748edc889484d2c
codeforany <130132429+codeforany@users.noreply.github.com>
2023-09-05 09:01:12 +0530

    Initial Commit

---
+++ social_swiftui/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  social_swiftui
+//
+//  Created by CodeForAny on 05/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ social_swiftui/social_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  social_swiftuiApp.swift
+//  social_swiftui
+//
+//  Created by CodeForAny on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct social_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
